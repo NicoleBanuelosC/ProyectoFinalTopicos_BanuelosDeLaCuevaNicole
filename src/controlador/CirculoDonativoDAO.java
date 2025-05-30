661840309f5e7c4b2cd3b8d55103920a88669dfb
Nicole Bañuelos <nicole.bc@tecjerez.edu.mx>
2025-05-29 22:31:16 -0600

    ajuste los DAO de llamador, coordinador y circulo, para que solo obtenga los id

--- src/controlador/CirculoDonativoDAO.java
+++ src/controlador/CirculoDonativoDAO.java
@@ -1,13 +1,8 @@
 package controlador;
 
-import modelo.CirculoDonativo;
 import java.sql.SQLException;
 import java.util.List;
 
 public interface CirculoDonativoDAO {
-    void alta(CirculoDonativo circulo) throws SQLException;
-    void baja(String idCirculo) throws SQLException;
-    void cambio(CirculoDonativo circulo) throws SQLException;
-    CirculoDonativo consulta(String idCirculo) throws SQLException;
-    List<CirculoDonativo> consultaTodos() throws SQLException;
+    List<String> obtenerIds() throws SQLException;
 }//CirculoDonativoDAO
--- src/controlador/CoordinadorClaseDAO.java
+++ src/controlador/CoordinadorClaseDAO.java
@@ -1,13 +1,8 @@
 package controlador;
 
 import java.sql.SQLException;
-import modelo.CoordinadorClase;
 import java.util.List;
 
 public interface CoordinadorClaseDAO {
-    void alta(CoordinadorClase coordinador) throws SQLException;
-    void baja(String idCoordinador) throws SQLException;
-    void cambio(CoordinadorClase coordinador) throws SQLException;
-    CoordinadorClase consulta(String idCoordinador) throws SQLException;
-    List<CoordinadorClase> consultaTodos() throws SQLException;
+    List<String> obtenerIds() throws SQLException;
 }//CoordinadorClaseDAO
